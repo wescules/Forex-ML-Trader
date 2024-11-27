a2d98dfb7515d5d0127c55534780240adaa31044
wescules <wafermonster@yahoo.com>
2024-11-27 16:05:27 +0700

    added temp config

---
+++ backend/config.py
@@ -0,0 +1,41 @@
+# contains settings and common lists of assets to trade
+
+# basic account/environment details
+accountID = '<insert-account_id>'
+token = '<insert-account_token>'
+env = 'practice'  # change this to 'live' when you're ready
+
+# major portfolios
+
+# currencies = ['EUR_USD', 'GBP_USD', 'USD_JPY', 'AUD_USD', 'NZD_USD',
+#               'USD_CHF', 'USD_CAD', 'XAU_USD', 'AUD_JPY', 'EUR_JPY',
+#               'GBP_JPY']
+
+currencies = [
+              "AUD_CAD",
+              "AUD_CHF",
+              "AUD_USD",
+              "CAD_CHF",
+              "CAD_SGD",
+              "EUR_AUD",
+              "EUR_CAD",
+              "EUR_CHF",
+              "EUR_GBP",
+              "EUR_JPY",
+              "EUR_SGD",
+              "EUR_USD",
+              "GBP_JPY",
+              "GBP_USD",
+              "NZD_USD",
+              "USD_CAD",
+              "USD_CHF",
+              "USD_THB",
+              ]
+
+indicies = ['SPX500_USD', 'AU200_AUD', 'CN50_USD', 'EU50_EUR', 'FR40_EUR',
+            'DE30_EUR', 'HK33_HKD', 'IN50_USD', 'JP225_USD', 'NL25_EUR',
+            'SG30_SGD', 'TWIX_USD', 'UK100_GBP', 'US30_USD', 'NAS100_USD',
+            'US2000_USD']
+
+commodities = ['BCO_USD', 'XCU_USD', 'CORN_USD', 'NATGAS_USD', 'SOYBN_USD',
+               'SUGAR_USD', 'WTICO_USD', 'WHEAT_USD']
